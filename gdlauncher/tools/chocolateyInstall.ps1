15ad7b952d142d72fa2ecd8fcee0b30638aa68a9
Jelle Schroijen <jelle2510@users.noreply.github.com>
2022-01-03 16:22:21 +0100

    AU: 1 updated - gdlauncher [skip ci]

--- gdlauncher/tools/chocolateyInstall.ps1
+++ gdlauncher/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'gdlauncher'
   fileType      = 'EXE'
-  url           = 'https://github.com/gorilla-devs/GDLauncher/releases/download/v1.1.20/GDLauncher-win-setup.exe'
-  checksum      = 'deed87cdc8be94e6de03dd6594f95c68440ff949775ceec9da130c68f409f1b4'
+  url           = 'https://github.com/gorilla-devs/GDLauncher/releases/download/v1.1.21/GDLauncher-win-setup.exe'
+  checksum      = 'df6b8afa5a12788f48d1e8cb808abe8657c76d837caae969f877600172859176'
   checksumType  = 'sha256'
   silentArgs    = "/qn /norestart"
   validExitCodes= @(0, 3010, 1641)
